29bf6767a8f87407b10f4df086a40a9f8b2934e7
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-05-17 22:46:03 +0700

    Add files via upload

---
+++ TestEFCoreMySQL/TestEFCoreMySQL/Utils/ConsoleHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Utils
+{
+    public static class ConsoleHelper
+    {
+        public static void ConsolePuase()
+        {
+            Console.WriteLine("Нажмите ВВОД для продолжения ...");
+            Console.ReadLine();
+        }
+    }
+}
