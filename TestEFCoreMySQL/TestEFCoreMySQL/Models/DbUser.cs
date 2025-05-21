c0efc287854b764a8fed6257eacf95df253c49e4
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-05-21 15:34:57 +0700

    Delete TestEFCoreMySQL directory

--- TestEFCoreMySQL/TestEFCoreMySQL/Models/DbUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestEFCoreMySQL.Models
-{
-    public class DbUser
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-    }
-}
